9d9ae9aa1fe2879eaa3389d76d7798bb64b773bf
alirashidAR <alirashid.38@yahoo.com>
2025-01-15 19:43:58 +0530

    refactor: add collecting script

--- src/open_data_pvnet/scripts/collect_pvlive_data.py
+++ src/open_data_pvnet/scripts/collect_pvlive_data.py
@@ -15,9 +15,13 @@ start = datetime(2020, 1, 1, 0, 0, 0, 0, tzinfo=pytz.UTC)
 end = datetime(2025, 1, 1, 1, 0, 0, 0, tzinfo=pytz.UTC)
 
 data = pv.get_data_between(start=start, end=end, extra_fields="capacity_mwp")
+<<<<<<< HEAD
 
 df = pd.DataFrame(data)
 print(df.head())
+=======
+df = pd.DataFrame(data)
+>>>>>>> a7d96d1 (refactor: add collecting script)
 
 df['datetime_gmt'] = pd.to_datetime(df['datetime_gmt'], utc=True)
 df['datetime_gmt'] = df['datetime_gmt'].dt.tz_convert(None)
@@ -29,7 +33,10 @@ ds['datetime_gmt'] = ds['datetime_gmt'].astype(np.datetime64)
 local_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'target_data.nc')
 
 os.makedirs(os.path.dirname(local_path), exist_ok=True)
+<<<<<<< HEAD
 
+=======
+>>>>>>> a7d96d1 (refactor: add collecting script)
 ds.to_netcdf(local_path)
 
 logger.info(f"Data successfully stored in {local_path}")
